4bebf90bdacd4e70d384148de2b6489b93998c3e
casperliuliuliu <casperliu1118@gmail.com>
2023-10-18 14:54:36 +0800

    add Vit in model_structure

--- C_model_structure.py
+++ C_model_structure.py
@@ -1,7 +1,9 @@
 from torchvision import datasets, models, transforms
 import torch.nn as nn
 import torch
-
+import timm
+resnet_list = ['resnet18','resnet101','resnet152']
+vit_list = ['vit_large']
 def get_model_structure(model_name, pretrain=None):
     if model_name == 'resnet18':
         return models.resnet18(weights=pretrain)
@@ -9,6 +11,8 @@ def get_model_structure(model_name, pretrain=None):
         return models.resnet101(weights=pretrain)
     elif model_name == 'resnet152':
         return models.resnet152(weights=pretrain)
+    elif model_name == 'vit_large':
+        return timm.create_model("vit_large_patch16_224", pretrained=pretrain)
     return None
     
 def get_model(model_name, pretrain, class_counts, pretrain_category):
@@ -22,8 +26,14 @@ def get_model(model_name, pretrain, class_counts, pretrain_category):
         print(f"Weight Loaded up successfully!")
         
     else:
-        model = get_model_structure(model_name, pretrain)
-        num_ftrs = model.fc.in_features
-        model.fc = nn.Linear(num_ftrs, len(class_counts))
+        if model_name in resnet_list:
+            model = get_model_structure(model_name, pretrain)
+            num_ftrs = model.fc.in_features
+            model.fc = nn.Linear(num_ftrs, len(class_counts))
 
+        elif model_name in vit_list:
+            model = get_model_structure(model_name, pretrain)
+            num_ftrs = model.head.in_features
+            model.head = nn.Linear(num_ftrs, len(class_counts))
+            
     return model
