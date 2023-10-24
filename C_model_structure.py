52aefeebb8d58a4573aebf48062a2608e0b24ba8
casperliuliuliu <casperliu1118@gmail.com>
2023-10-24 22:07:53 +0800

    writing ensemble model

--- C_model_structure.py
+++ C_model_structure.py
@@ -6,11 +6,29 @@ from MedViT import MedViT_small
 from MedViT import MedViT_base
 from MedViT import MedViT_large
 
+class EnsembleModel(nn.Module):
+    def __init__(self, base_models):
+        super(EnsembleModel, self).__init__()
+        self.base_models = nn.ModuleList(base_models)
+
+    def forward(self, x):
+        predictions = []
+        for model in self.base_models:
+            model.eval()
+            with torch.no_grad():
+                output = model(x)
+            predictions.append(output)
+
+        # Combine the predictions (averaging in this case)
+        ensemble_output = torch.mean(torch.stack(predictions), dim=0)
+        return ensemble_output
+
 resnet_list = ['resnet18', 'resnet101', 'resnet152']
 resnet_mod_list = ['resnet18_mod1', 'resnet101_mod1', 'resnet152_mod1']
 densenet_list = ['densenet121', 'densenet161', 'densenet169', 'densenet201']
 vit_list = ['vit_small', 'vit_base', 'vit_large']
 medvit_list = ['medvit_small', 'medvit_base', 'medvit_large']
+
 def get_model_structure(model_name, pretrain=None):
     if model_name == 'resnet18' or model_name == 'resnet18_mod1':
         return models.resnet18(weights=pretrain)
@@ -41,7 +59,9 @@ def get_model_structure(model_name, pretrain=None):
     return None
     
 def get_model(model_name, pretrain, class_counts, pretrain_category, dropout_prob):
-    if isinstance(pretrain, str): # using my own pretrained weight.
+    if isinstance(model_name, list):
+        model = EnsembleModel(model_name)
+    elif isinstance(pretrain, str): # using my own pretrained weight.
         print(f"Loading up your own model weight:{pretrain}")
         model = get_model_structure(model_name)
         num_ftrs = model.fc.in_features
--- train_3090.py
+++ train_3090.py
@@ -1,105 +1,62 @@
 from C_BASELINE import train_mod
 import torch
 from C_other_func import Notification
-name = "diff_dataset_1020"
-path = "D:/REDO/RESNET152/"
-dropout_prob = 0
-ii= 1
-model_things = {
-    'data_dir' : "D:/P2023/DATA/glomer_cg",
-    'train_ratio' : 0.6,
-    'val_ratio' : 0.5,
-    'random_seed' : 42,
-    'batch_size' : 40,
-    'log_path' : f"{path}{name}.txt",
-    'weight_store_path' : f"{path}/WEIGHT/{name}({ii}).pt",
-    'learning_rate' : 0.01,
-    'num_of_epoch' : 20,
-    'lr_method' : "LR_stepping",
-    'pretrain' : True,
-    'pretrain_category' : None,
-    'model_name' : 'resnet152',
-    'other_info' : "Try to test model and see the result of classifying in different dataset. \nHope it goes well",
-    'data_transforms_op' : 2,
-    'dropout_prob' :  dropout_prob
-}
-model = train_mod(model_things)
-weight_store_path = model_things['weight_store_path']
-torch.save(model.state_dict(), weight_store_path)
+# train_list = ['resnet152', 'resnet152', 'resnet101', 'resnet101',]
+# data_list = ['glomer_cg', 'glomer_tvgh', 'glomer_cg', 'glomer_tvgh',]
+# for ii in range(len(train_list)):
+#     mod_running = train_list[ii]
+#     data_running = data_list[ii]
+#     print(mod_running)
+#     name = "diff_dataset_1020"
+#     path = f"D:/REDO/{mod_running}/"
+#     dropout_prob = None
+#     model_things = {
+#         'data_dir' : f"D:/P2023/DATA/{data_running}",
+#         'train_ratio' : 0.6,
+#         'val_ratio' : 0.5,
+#         'random_seed' : 42,
+#         'batch_size' : 40,
+#         'log_path' : f"{path}{name}.txt",
+#         'weight_store_path' : f"{path}/WEIGHT/{name}({ii}).pt",
+#         'learning_rate' : 0.01,
+#         'num_of_epoch' : 20,
+#         'lr_method' : "LR_stepping",
+#         'pretrain' : True,
+#         'pretrain_category' : None,
+#         'model_name' : mod_running,
+#         'other_info' : "Try to test model and see the result of classifying in different dataset. \nHope it goes well",
+#         'data_transforms_op' : 2,
+#         'dropout_prob' :  dropout_prob,
+#     }
+#     model = train_mod(model_things)
+#     weight_store_path = model_things['weight_store_path']
+#     torch.save(model.state_dict(), weight_store_path)
 
-dropout_prob = 0
-ii= 2
-model_things = {
-    'data_dir' : "D:/P2023/DATA/glomer_tvgh",
-    'train_ratio' : 0.6,
-    'val_ratio' : 0.5,
-    'random_seed' : 42,
-    'batch_size' : 40,
-    'log_path' : f"{path}{name}.txt",
-    'weight_store_path' : f"{path}/WEIGHT/{name}({ii}).pt",
-    'learning_rate' : 0.01,
-    'num_of_epoch' : 20,
-    'lr_method' : "LR_stepping",
-    'pretrain' : True,
-    'pretrain_category' : None,
-    'model_name' : 'resnet152',
-    'other_info' : "Try to test model and see the result of classifying in different dataset. \nHope it goes well",
-    'data_transforms_op' : 2,
-    'dropout_prob' :  dropout_prob
-}
-model = train_mod(model_things)
-weight_store_path = model_things['weight_store_path']
-torch.save(model.state_dict(), weight_store_path)
-
-# model 1
-name = "diff_dataset_1020"
-path = "D:/REDO/RESNET101/"
-dropout_prob = 0
-ii= 1
-model_things = {
-    'data_dir' : "D:/P2023/DATA/glomer_cg",
-    'train_ratio' : 0.6,
-    'val_ratio' : 0.5,
-    'random_seed' : 42,
-    'batch_size' : 40,
-    'log_path' : f"{path}{name}.txt",
-    'weight_store_path' : f"{path}/WEIGHT/{name}({ii}).pt",
-    'learning_rate' : 0.01,
-    'num_of_epoch' : 20,
-    'lr_method' : "LR_stepping",
-    'pretrain' : True,
-    'pretrain_category' : None,
-    'model_name' : 'resnet101',
-    'other_info' : "Try to test model and see the result of classifying in different dataset. \nHope it goes well",
-    'data_transforms_op' : 2,
-    'dropout_prob' :  dropout_prob
-}
-model = train_mod(model_things)
-weight_store_path = model_things['weight_store_path']
-torch.save(model.state_dict(), weight_store_path)
-
-dropout_prob = 0
-ii= 2
-model_things = {
-    'data_dir' : "D:/P2023/DATA/glomer_tvgh",
-    'train_ratio' : 0.6,
-    'val_ratio' : 0.5,
-    'random_seed' : 42,
-    'batch_size' : 40,
-    'log_path' : f"{path}{name}.txt",
-    'weight_store_path' : f"{path}/WEIGHT/{name}({ii}).pt",
-    'learning_rate' : 0.01,
-    'num_of_epoch' : 20,
-    'lr_method' : "LR_stepping",
-    'pretrain' : True,
-    'pretrain_category' : None,
-    'model_name' : 'resnet101',
-    'other_info' : "Try to test model and see the result of classifying in different dataset. \nHope it goes well",
-    'data_transforms_op' : 2,
-    'dropout_prob' :  dropout_prob
-}
-model = train_mod(model_things)
-weight_store_path = model_things['weight_store_path']
-torch.save(model.state_dict(), weight_store_path)
-
-
+densenet_list = ['densenet121', 'densenet161', 'densenet169', 'densenet201']
+for ii in range(len(densenet_list)):
+    mod_running = densenet_list[ii]
+    print(mod_running)
+    name = "test_model_1020"
+    path = f"D:/REDO/{mod_running}/"
+    dropout_prob = None
+    model_things = {
+        'data_dir' : "D:/P2023/DATA/glomer_cg(2)",
+        'train_ratio' : 0.6,
+        'val_ratio' : 0.5,
+        'random_seed' : 42,
+        'batch_size' : 40,
+        'log_path' : f"{path}{name}.txt",
+        'weight_store_path' : f"{path}/WEIGHT/{name}({ii}).pt",
+        'learning_rate' : 0.01,
+        'num_of_epoch' : 20,
+        'lr_method' : "LR_stepping",
+        'pretrain' : True,
+        'pretrain_category' : None,
+        'model_name' : mod_running,
+        'other_info' : "Try to test densenet series acc, especially the difference between 161 and 169",
+        'data_transforms_op' : 2,
+        'dropout_prob' :  dropout_prob,
+    }
+    model = train_mod(model_things)
+    weight_store_path = model_things['weight_store_path']
+    torch.save(model.state_dict(), weight_store_path)
